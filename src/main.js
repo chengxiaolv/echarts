4435e8b85dd54a6542b3e3d20d0c207ff9731552
chengxiaolv <972987164@qq.com>
2019-03-08 09:48:48 +0800

    替换

--- src/main.js
+++ src/main.js
@@ -4,36 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-import https from "./utils/axios.js"; //引入封装的axios  
-Vue.prototype.https = https; //引入到原型上。
-
-import utils from './utils/utils'; // 引入全局注册的方法
-Vue.prototype.utils = utils; //引入到原型上。
-
-import * as filters from './utils/filters' // 注册全局过滤器
-Object.keys(filters).forEach(key => {
-    Vue.filter(key, filters[key]);
-})
-
-// 引入图片预览功能。
-import 'viewerjs/dist/viewer.css'
-import Viewer from 'v-viewer'
-Vue.use(Viewer)
-
-// 引入element-ui组件
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(ElementUI);
-
-
-Vue.component('tabBarList', resolve => require(['./common_assemblys/tabBarList.vue'], resolve)); // 状态列表（比如待处理、进行中、已完成等）
-
-
 Vue.config.productionTip = false
-    /* eslint-disable no-new */
+
+/* eslint-disable no-new */
 new Vue({
-    el: '#app',
-    router,
-    components: { App },
-    template: '<App/>'
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import common_assemblys_example from '@/common_assemblys_example/common_assemblys_example' //组件实列文件集合。
 
 Vue.use(Router)
 
 export default new Router({
-    routes: [{
-        path: '/',
-        name: 'common_assemblys_example',
-        component: common_assemblys_example
-    }, {
-        path: '/HelloWorld',
-        name: 'HelloWorld',
-        component: HelloWorld
-    }, { // 组件的示例页面。
-        path: '/common_assemblys_example',
-        name: 'common_assemblys_example',
-        component: common_assemblys_example
-    }]
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
 })
